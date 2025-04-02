df76c03baa24553897f878cd8ccf99b5938fc824
singhpulkit <33547313+singhpulkit@users.noreply.github.com>
2025-04-02 13:57:28 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "Singhpulkit-org"
+
+    workspaces {
+      name = "demo-workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
